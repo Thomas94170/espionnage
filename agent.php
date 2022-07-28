0ff41c7d35a5fa667cd4c7c9ed811f756e784806
Thomas Thirion <thirionthomas68100@gmail.com>
2022-07-28 16:15:33 +0200

    connect bdd agent

--- agent.php
+++ agent.php
@@ -52,8 +52,17 @@
 
             <?php
 
+            $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $cleardb_server = $cleardb_url["host"];
+            $cleardb_username = $cleardb_url["user"];
+            $cleardb_password = $cleardb_url["pass"];
+            $cleardb_db = substr($cleardb_url["path"], 1);
+            $active_group = 'default';
+            $query_builder = TRUE;
+
+
             try {
-                $pdo = new PDO('mysql:host=localhost;dbname=espionstudi', 'root', '');
+                $pdo = new PDO($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db());
                 if (isset($_GET['searchA']) && !empty(trim($_GET['searchAgent']))) {
                     $sql = "SELECT * FROM agents WHERE name = '$_GET[searchAgent]'";
                 } else {
