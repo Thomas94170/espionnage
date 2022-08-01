638267d93f2b22a7f428e60f934597e44236e970
Thomas Thirion <thirionthomas68100@gmail.com>
2022-08-01 14:42:56 +0200

    !

--- updateAgent.php
+++ updateAgent.php
@@ -150,7 +150,7 @@
             nationality = '$_POST[majNat]' 
             WHERE id = '$_GET[update]'";
             foreach (mysqli_query($pdo, ("SELECT * from agents WHERE name = '$_POST[name]'")) as $agent) {
-                $sql2 = "UPDATE skillagent SET skill_id = '$_POST[speciality]' WHERE agent_id = '$agent[id]'";
+                $sql2 = "UPDATE skillagent SET skill_id = '$_POST[skill_id]' WHERE agent_id = '$agent[id]'";
             }
             mysqli_query($pdo, $sql);
             mysqli_query($pdo, $sql2);
