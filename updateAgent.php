a206538d55ffee034f3babf110c0671591a0aef7
Thomas Thirion <thirionthomas68100@gmail.com>
2022-08-02 10:15:42 +0200

    !

--- updateAgent.php
+++ updateAgent.php
@@ -149,10 +149,10 @@
             authentificationCode = '$_POST[majCode]', 
             nationality = '$_POST[majNat]' 
             WHERE id = '$_GET[update]'";
+            mysqli_query($pdo, $sql);
             foreach (mysqli_query($pdo, ("SELECT * from agents WHERE name = '$_POST[name]'")) as $agent) {
                 $sql2 = "UPDATE skillagent SET skill_id = '$_POST[skill_id]' WHERE agent_id = '$agent[id]'";
             }
-            mysqli_query($pdo, $sql);
             mysqli_query($pdo, $sql2);
         }
     } catch (PDOException $e) {
